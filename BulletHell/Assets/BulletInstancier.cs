e638483599d80ff5dd24525c80a02e0e06422581
sneakysunset <andrelauzier@gmail.com>
2022-04-16 13:01:59 +0200

    a

---
+++ BulletHell/Assets/BulletInstancier.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BulletFury;
+
+public class BulletInstancier : MonoBehaviour
+{
+    public float spawnTimer;
+    public BulletManager BulletSpawner1;
+
+    void Awake()
+    {
+        //BulletSpawner1.Spawn(transform.position, forward: BulletSpawner1.Plane == BulletFury.Data.BulletPlane.XY ? transform.up : transform.forward);
+
+    }
+
+    void Start()
+    {
+        
+        //StartCoroutine(BulletSpawn(spawnTimer));
+        
+    }
+
+    void Update()
+    {
+        BulletSpawner1.Spawn(transform.position, forward: BulletSpawner1.Plane == BulletFury.Data.BulletPlane.XY ? transform.up : transform.forward);
+    }
+
+    IEnumerator BulletSpawn(float timer)
+    {
+
+        BulletSpawner1.Spawn(transform.position, forward: BulletSpawner1.Plane == BulletFury.Data.BulletPlane.XY ? transform.up : transform.forward);
+
+        yield return new WaitForSeconds(timer);
+
+        StartCoroutine(BulletSpawn(timer));
+    }
+
+    public void Printer()
+    {
+        print(1);
+    }
+}
