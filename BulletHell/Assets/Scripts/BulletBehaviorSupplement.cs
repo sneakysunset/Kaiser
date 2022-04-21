d1cc7838b21c03c70bc33c201ca5e9d1a0ec84ec
sneakysunset <andrelauzier@gmail.com>
2022-04-21 15:31:02 +0200

    Bomb

--- BulletHell/Assets/Scripts/BulletBehaviorSupplement.cs
+++ BulletHell/Assets/Scripts/BulletBehaviorSupplement.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BulletFury;
+using BulletFury.Data;
+
 
 public class BulletBehaviorSupplement : MonoBehaviour
 {
@@ -10,7 +13,7 @@ public class BulletBehaviorSupplement : MonoBehaviour
     [SerializeField] public float lookAtDelay;
     [SerializeField] public Transform Target;
     Vector3 targetPos;
-
+    public BulletManager BombBullets;
 
     private void Start()
     {
@@ -20,21 +23,32 @@ public class BulletBehaviorSupplement : MonoBehaviour
 
     void Update()
     {
-         targetPos = new Vector3(Target.position.x, transform.position.y, Target.position.z);
+        if(Target!=null)
+            targetPos = new Vector3(Target.position.x, transform.position.y, Target.position.z);
 
         if (rotate)
             transform.Rotate(0, Time.deltaTime * rotateSpeed, 0);
 
-        if (lookAtTarget && lookAtDelay == 0)
-            transform.LookAt(Target.position);
+        if(Target != null)
+            if (lookAtTarget && lookAtDelay == 0)
+                transform.LookAt(Target.position);
     }
 
     IEnumerator lookAtDelayer(float timer)
     {
         yield return new WaitForSeconds(timer);
-        if(lookAtTarget)
-            transform.LookAt(targetPos);
+        if (Target != null)
+            if (lookAtTarget)
+                transform.LookAt(targetPos);
 
         StartCoroutine(lookAtDelayer(lookAtDelay));
+    }
+
+    public void BombEffect(int a, BulletContainer bulletContainer, bool isDeadFromTime)
+    {
+        if (isDeadFromTime)
+        {
+            BombBullets.Spawn(bulletContainer.Position, forward: BombBullets.Plane == BulletFury.Data.BulletPlane.XY ? transform.up : transform.forward);
+        }
     }
 }
