58e370be09cc30b9aa5841ab1c6ee32bfaf67db4
sneakysunset <andrelauzier@gmail.com>
2022-04-23 23:27:38 +0200

    qweqw

---
+++ BulletHell/Assets/SoundPlayer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BulletFury.Data;
+
+public class SoundPlayer : MonoBehaviour
+{
+   public static void PlayOneShot(string eventName)
+   {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/" + eventName);
+   }
+
+    public void PlayOneShotStar(int i, BulletContainer bC)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Ennemy/Etoile/Tir");
+    }
+
+    public void PlayOneShotPoulpe(int i, BulletContainer bC)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Ennemy/Poulpe/Tir");
+    }
+
+    public void PlayOneShotRaie(int i, BulletContainer bC)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Ennemy/Raie/Tir");
+    }
+
+    public void PlayOneShotBomb(int i, BulletContainer bC)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Ennemy/Bombe/Tir");
+    }
+}
